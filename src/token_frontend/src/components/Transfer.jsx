6dfd6da78df133cfcbe20452a3620d0dfbcdda53
Elina Singh <elinasingh1997@gmail.com>
2024-03-15 20:39:20 +0530

    transfer money from canister to claimant

--- src/token_frontend/src/components/Transfer.jsx
+++ src/token_frontend/src/components/Transfer.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Principal } from "../../../../node_modules/@dfinity/principal/lib/cjs/index";
+import { token_backend } from "../../../declarations/token_backend";
 
 function Transfer() {
+
+  const [recipientId, setId] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isDisabled, setDisable] = useState(false);
+  const [feedback, setFeedback] = useState("");
+  const [isHidden, setHidden] = useState(true);
   
   async function handleClick() {
-    
+    setHidden(true);
+    setDisable(true);
+    const recipient = Principal.fromText(recipientId);
+    const amountToTransfer = Number(amount);
+    const result = await token_backend.transfer(recipient, amountToTransfer);
+    setFeedback(result);
+    setHidden(false);
+    setDisable(false);
   }
 
   return (
@@ -16,6 +31,8 @@ function Transfer() {
               <input
                 type="text"
                 id="transfer-to-id"
+                value={recipientId}
+                onChange={(e) => setId(e.target.value)}
               />
             </li>
           </ul>
@@ -27,15 +44,22 @@ function Transfer() {
               <input
                 type="number"
                 id="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
               />
             </li>
           </ul>
         </fieldset>
         <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
+          <button 
+          id="btn-transfer" 
+          onClick={handleClick} 
+          disabled={isDisabled}
+          >
             Transfer
           </button>
         </p>
+        <p hidden={isHidden}>{feedback}</p>
       </div>
     </div>
   );
