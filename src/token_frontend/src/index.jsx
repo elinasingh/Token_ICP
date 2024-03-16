33418a53c9186463c912ba6e6d00ef6eca81d31a
Elina Singh <elinasingh1997@gmail.com>
2024-03-16 21:40:04 +0530

    Adding auth-client for authenticate users

--- src/token_frontend/src/index.jsx
+++ src/token_frontend/src/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
 
 const root = createRoot(document.getElementById("root"));
+
+const authClient = await AuthClient.create();  
 
 root.render(
   <App />
 );
+
+
