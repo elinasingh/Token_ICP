926b15f63c325b375cd67e4e6ded6aee43614605
Elina Singh <elinasingh1997@gmail.com>
2024-03-25 21:37:36 +0530

    Authentication using dfinity

--- src/token_frontend/src/index.jsx
+++ src/token_frontend/src/index.jsx
@@ -3,12 +3,34 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import { AuthClient } from "@dfinity/auth-client";
 
-const root = createRoot(document.getElementById("root"));
+const init = async () => {
+  try {
+    const authClient = await AuthClient.create(); 
 
-const authClient = await AuthClient.create();  
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+      const root = createRoot(document.getElementById("root"));
+      root.render(
+        <App />
+      );
+    }
+  });
+} catch (error) {
+  console.error("Error initializing aithentication: ", error);
+}
+};
 
-root.render(
-  <App />
-);
+
+init();
 
 
+ 
+
+
+
+
+
+
+
+  
